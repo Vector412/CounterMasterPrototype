39848a96a5935c9ab5ca4e9786130da12f774264
MaksimTsurko <36657655+MaksimTsurko@users.noreply.github.com>
2021-08-13 02:30:37 +0200

    [maksim] nihuia novogo

--- CountMaster/Assets/Scripts/Obstacles/Axe_Controller.cs
+++ CountMaster/Assets/Scripts/Obstacles/Axe_Controller.cs
@@ -29,14 +29,15 @@ public class Axe_Controller : MonoBehaviour, IObstacles
     {
         // Rotate the object around its local X axis at 1 degree per second
 
-    
-        transform.Rotate(Vector3.up , 10 * Time.deltaTime);
+        transform.Rotate(0, (Mathf.PingPong(Time.time , 1.571f) - 3.92699f) /** _speed*/, 0);
+        Debug.Log(Mathf.Rad2Deg*(Mathf.PingPong(Time.time, 1.571f) - 3.92699f));
+        //transform.Rotate(Vector3.up , 10 * Time.deltaTime);
         
-        Vector3 axeEulerAngels = localTransform.rotation.eulerAngles;
-        Debug.Log(axeEulerAngels);
-        axeEulerAngels.y = (axeEulerAngels.y > 180) ? axeEulerAngels.y - 360 : axeEulerAngels.y;
-        axeEulerAngels.y = Mathf.Clamp(axeEulerAngels.y, 45, -45)* Time.deltaTime;
-        localTransform.rotation = Quaternion.Euler(axeEulerAngels);
+        //Vector3 axeEulerAngels = localTransform.rotation.eulerAngles;
+        //Debug.Log(axeEulerAngels);
+        //axeEulerAngels.y = (axeEulerAngels.y > 180) ? axeEulerAngels.y - 360 : axeEulerAngels.y;
+        //axeEulerAngels.y = Mathf.Clamp(axeEulerAngels.y, 45, -45)* Time.deltaTime;
+        //localTransform.rotation = Quaternion.Euler(axeEulerAngels);
         /*LimitRot();*/
        /*transform.localRotation = Quaternion.Euler(0, yRot, 0); // This clamps the xRotation (looking up and down) to a minimum of -90 degrees and a maximum of +90 degrees.*/
      
